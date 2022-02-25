f43abd31027000c4d9b767f12877c030a8fd9355
Rhett <jhsyzxy@gmail.com>
2022-02-25 18:03:29 +0800

    新增 if else 和三元条件运算表达式的测试

--- runoob/commandOut/src/main.rs
+++ runoob/commandOut/src/main.rs
@@ -1,29 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let a = -12;
+    let b;
+    if a > 0 {
+        b = 1;
+    }else {
+        b = -1
+    }
 
-    another_function();
-    another_function_para(12, 34);
-
-    let x = 5;
-
-    let y = {
-        let x = 3;
-        x + 1
-    };
-
-    println!("x 的值为: {}",x);
-    println!("y 的值为: {}",y);
-
-    fn five()->i32 {5}
-
-    println!("five() 的值为: {}", five());
-}
-
-fn another_function() {
-    println!("Hello, world!");
-}
-
-fn another_function_para(x: i32, y: i32) {
-    println!("x 的值为: {}", x);
-    println!("y 的值为: {}", y);
+    let number = if a > 0 {10} else {-10};
+    println!("b的值为: {}", b);
+    println!("number的值为: {}", number);
 }
