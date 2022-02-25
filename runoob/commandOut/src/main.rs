14b75205b04846f0e599c92816a9314e3ca93ede
Rhett <jhsyzxy@gmail.com>
2022-02-25 17:51:42 +0800

    新增rust函数不同的写法

--- runoob/commandOut/src/main.rs
+++ runoob/commandOut/src/main.rs
@@ -1,7 +1,29 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
-    let x = x * 2;
+    println!("Hello, world!");
 
-    println!("The value of x is:{}", x)
+    another_function();
+    another_function_para(12, 34);
+
+    let x = 5;
+
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("x 的值为: {}",x);
+    println!("y 的值为: {}",y);
+
+    fn five()->i32 {5}
+
+    println!("five() 的值为: {}", five());
+}
+
+fn another_function() {
+    println!("Hello, world!");
+}
+
+fn another_function_para(x: i32, y: i32) {
+    println!("x 的值为: {}", x);
+    println!("y 的值为: {}", y);
 }
