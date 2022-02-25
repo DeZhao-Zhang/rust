b29da7873b27aa061fb825251fd91fdc2bf5f4ef
Rhett <jhsyzxy@gmail.com>
2022-02-25 15:21:35 +0800

    1. 项目初始化 2.开始菜鸟教程的学习 3.完成hello world

---
+++ runoob/greeting/target/debug/deps/greeting.d
@@ -0,0 +1,5 @@
+D:\rhett\code\rust\runoob\greeting\target\debug\deps\greeting.exe: src\main.rs
+
+D:\rhett\code\rust\runoob\greeting\target\debug\deps\greeting.d: src\main.rs
+
+src\main.rs:
---
+++ runoob/greeting/target/debug/greeting.d
@@ -0,0 +1,1 @@
+D:\rhett\code\rust\runoob\greeting\target\debug\greeting.exe: D:\rhett\code\rust\runoob\greeting\src\main.rs
